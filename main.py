5594433e833d2eeba5cc28ba6ebc2104f3a9cb3a
Nadav Gover <nadavgover@gmail.com>
2019-09-08 16:09:49 +0300

    added command line user interface to fetch source and destination articles

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from wiki_game import WikiGame
 from WikiGameError import WikiGameError
 import time
 
+def get_user_input():
+    print("For best results and avoiding disambiguation,\n"
+          "it's better to go to Wikipedia and copy the exact name of the article.\n")
+    source = input("Enter source article:")
+    destination = input("Enter destination article: ")
+    return source, destination
+
 
 def main():
     try:
-        wiki = WikiGame(source="Breadth-first search", destination="Computer science")
+        source, destination = get_user_input()
+        wiki = WikiGame(source=source, destination=destination)
+        print("Solving, this might take a few seconds\n")
         start_time = time.time()
         wiki.solve()
         elapsed_time = time.time() - start_time
