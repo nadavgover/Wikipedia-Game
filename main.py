11f3d161ccdc9b63519f6b0f2f74d634ec8fc0ae
Nadav Gover <nadavgover@gmail.com>
2019-09-18 16:52:50 +0300

    Changed to print the time only 3 decimal digits

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import time
 
 def main():
     try:
-        wiki = WikiGame(source="Breadth-first search", destination="Computer science")
+        wiki = WikiGame(source="Breadth-first search", destination="computer science")
         start_time = time.time()
         wiki.solve()
         elapsed_time = time.time() - start_time
         print("The shortest path is:")
         wiki.print_path()
         print("")
-        print("Found a path in {} seconds, which are {} minutes".format(elapsed_time, elapsed_time / 60.0))
+        print("Found a path in {:.3f} seconds, which are {:.3f} minutes".format(elapsed_time, elapsed_time / 60.0))
 
     except WikiGameError as e:
         print(e)
